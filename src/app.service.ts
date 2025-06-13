c82bee8184b991a7de48f6458285d8180108b6e2
ElPlokiPloki <josefranciscox3@gmail.com>
2025-06-12 21:22:39 -0500

    Hice el método resta

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  
+  suma(a:number , b:number): number{
+    return a - b;
   }
 }
