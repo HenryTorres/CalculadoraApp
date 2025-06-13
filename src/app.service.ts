b5083e4b99a61ea0e2d09076e25508d0947ec39b
anariveraramirez03@gmail.com <Anariveraramirez03@gmail.com>
2025-06-12 21:33:13 -0500

    funcion suma

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,13 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  functionSuma(a: number, b: number): number {
+    return a+b;
+    
+    let rpta = functionSuma(5, 2);
+    console.log(rpta);
+  }
+
+ 
 }
